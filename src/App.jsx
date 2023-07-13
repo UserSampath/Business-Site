b2bfea93b992a41d3b3583781223b5ef52323440
UserSampath <nalakasampathsmp@gmail.com>
2023-07-13 09:06:44 +0530

    add hero part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 import "./App.css";
+import { Companies } from "./components/Companies/Companies";
+import { Residencies } from "./components/Residencies/Residencies";
 function App() {
   return (
     <div className="app">
@@ -9,6 +11,8 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
+      <Residencies/>
     </div>
   );
 }
