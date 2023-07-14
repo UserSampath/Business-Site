4865fbfe061950cdad0ef321a13086f2f8a8c733
UserSampath <nalakasampathsmp@gmail.com>
2023-07-14 17:04:45 +0530

    slider complete

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "swiper/css";
 import "./Residencies.css";
 import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/common";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 export const Residencies = () => {
   return (
@@ -11,22 +12,33 @@ export const Residencies = () => {
           <span className="orangeText">Best Choices</span>
           <span className="primaryText">Popular Residencies</span>
         </div>
-        <Swiper>
+        <Swiper {...sliderSettings}>
+          <SliderButton />
           {data.map((card, i) => (
             <SwiperSlide key={i}>
-              <div className="r-card">
+              <div className="r-card flexColStart">
                 <img src={card.image} alt="home" />
                 <span className="secondaryTest r_price">
                   <span style={{ color: "orange" }}>$</span>
                   <span>{card.price}</span>
                 </span>
-                <span classname="primaryText">{card.name}</span>
-                <span classname="secondaryText">{card.detail}</span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
     </section>
+  );
+};
+
+const SliderButton = () => {
+  const swiper = useSwiper();
+  return (
+    <div className="r-buttons flexCenter">
+      <button onClick={() => swiper.slidePrev()}>&lt;</button>
+      <button onClick={() => swiper.slideNext()}>&gt;</button>
+    </div>
   );
 };
---
+++ src/utils/common.js
@@ -0,0 +1,12 @@
+export const sliderSettings = {
+    sliderPerView: 1,
+    spaceBetween: 50,
+    breakpoints: {
+        80: { slidesPerView: 1 },
+        60:{ slidesPerView: 2},
+        75: { slidesPerView: 3 },
+        110: { slidesPerView: 4 },
+
+        
+    }
+}
